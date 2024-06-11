75d309c0cff1ffe836bf964e56d651b137e397a1
Kai Inui <lied.der.optik@gmail.com>
2024-06-12 00:32:06 +0900

    Add WebApp functionality

--- bin/deploy.ts
+++ bin/deploy.ts
@@ -3,4 +3,16 @@ import { prepareClaspJson } from "./prepare-clasp-json";
 
 await prepareClaspJson();
 await $`bun run build`;
-await $`bunx clasp deploy`;
+
+const deploymentId = await getLastDeploymentId();
+console.log(`Last deployment ID: ${deploymentId}`);
+await $`bunx clasp deploy -i ${deploymentId} --description "Deployed at $(date)"`;
+
+async function getLastDeploymentId() {
+	const result = await $`bunx clasp deployments`.text();
+	const lines = result.trim().split("\n");
+	const deploymentId = lines.pop()?.split(" ")[1];
+	return deploymentId;
+}
+
+
